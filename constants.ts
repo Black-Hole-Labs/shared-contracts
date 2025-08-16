e39884e4c5abdba58806ff12effb00f2b5873b4c
esempe <steam.dota.return@gmail.com>
2025-08-16 19:37:36 +0200

    add all chains

--- constants.ts
+++ constants.ts
@@ -7,6 +7,26 @@ export const getDiamondAddress = (chainId: CHAIN_IDS) => {
       return 'not deployed';
     case CHAIN_IDS.BASE:
       return BaseDiamond;
+    case CHAIN_IDS.ETHEREUM:
+      return 'not deployed';
+    case CHAIN_IDS.BASE:
+      return BaseDiamond;
+    case CHAIN_IDS.ARBITRUM:
+      return 'not deployed';
+    case CHAIN_IDS.BSC:
+      return 'not deployed';
+    case CHAIN_IDS.POLYGON:
+      return 'not deployed';
+    case CHAIN_IDS.MANTLE:
+      return 'not deployed';
+    case CHAIN_IDS.METIS:
+      return 'not deployed';
+    case CHAIN_IDS.SEI:
+      return 'not deployed';
+    case CHAIN_IDS.LINEA:
+      return 'not deployed';
+    case CHAIN_IDS.OPTIMISM:
+      return 'not deployed';
     default:
       throw new Error(`Unsupported chainId: ${chainId}`);
   }
